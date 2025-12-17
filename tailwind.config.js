88803f07423e033e474391ac8a537b94461576d7
nourromdhani789 <nour.romdhani@esprit.tn>
2025-12-17 16:28:23 +0100

    solve conflict

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  content: ["./src/**/*.{html,js}", "./public/*.html"],
   content: [
     "./public/**/*.html",
     "./src/**/*.css",
@@ -8,6 +9,10 @@ module.exports = {
   theme: {
     extend: {
       colors: {
+        primary: "#1A3D64",
+        secondary: "#0C2B4E",
+        accent: "#1D546C",
+        neutrallight: "#F4F4F4",
         primary: "#1a3d64",
         secondary: "#0c2b4e",
         accent: "#1d546c",
